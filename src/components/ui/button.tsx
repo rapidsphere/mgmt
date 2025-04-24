472da5a78664cd09061d80f44394d0fc030e7ff6
Selva Ganesh <selva9176@gmail.com>
2025-04-24 16:47:04 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="primary">
    
    with the text content:
    
        Add to List
    
    Relevant files:
    
        - https://6000-idx-studio-1745313609480.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev/_next/static/chunks/src_63e993cb._.js
        - https://6000-idx-studio-1745313609480.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
        - https://6000-idx-studio-1745313609480.cluster-w5vd22whf5gmav2vgkomwtc4go.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    the  button color should be green

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        primary: "bg-green-500 text-white hover:bg-green-600",
       },
       size: {
         default: "h-10 px-4 py-2",
@@ -54,3 +55,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
+
